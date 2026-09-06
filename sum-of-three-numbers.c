fc90850fef0bc0d8ba01fb30150d3069bc6ada0b
Sayeem Ahmed <sayeemsayeem58@gmail.com>
2026-09-07 02:27:46 +0600

    Rename sum of three numbers file

---
+++ sum-of-three-numbers.c
@@ -0,0 +1,10 @@
+// calculating sum of three number
+#include<stdio.h>
+int main() {
+      int a, b, c;
+      scanf("%d%d%d", &a, &b, &c);
+      int sum = a + b + c;
+      printf("%d", sum);
+      return 0;
+
+}
