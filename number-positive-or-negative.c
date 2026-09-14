165bc824ef36b4e1bf367b944b55f636ec76308b
Sayeem Ahmed <sayeemsayeem58@gmail.com>
2026-09-15 01:20:56 +0600

    Add number positivity check program
    Implement a program to check if a number is positive, negative, or zero.

---
+++ number-positive-or-negative.c
@@ -0,0 +1,20 @@
+checking if a number is positive or negative
+#include <stdio.h>
+
+int main() {
+    int num;
+    scanf("%d",&num);
+    
+    if("num > 0") {
+    printf("it is a positive number");
+    }
+    else if("num < 0") {
+    printf("it is a negative number");
+    }
+    else if("num == 0") {
+    printf("it's neither positive nor negative, it's zero");
+    }
+    
+    return 0;
+    
+}
