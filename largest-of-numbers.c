f2d61dc7f817c5a6c74336670a0dd499d38e2322
Sayeem Ahmed <sayeemsayeem58@gmail.com>
2026-09-15 01:53:48 +0600

    Implement program to find largest of three numbers

---
+++ largest-of-numbers.c
@@ -0,0 +1,19 @@
+//largest of three numbers
+#include <stdio.h>
+
+int main() {
+	int a, b, c;
+	scanf("%d %d %d", &a, &b, &c);
+	
+	if(a >= b && a >= c) {
+    printf("a is the largest number");
+	}
+	else if (b >= a && b >= c) {
+	printf("b is the largest number");
+	}
+	else if(c >= b && c >= a) {
+	printf("c is the largest number");
+	}
+	return 0;
+
+}
